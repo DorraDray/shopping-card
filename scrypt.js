90ff85c056d0d967e084f7b5ed3365b967c0eb8a
DorraDray <60601319+DorraDray@users.noreply.github.com>
2020-02-19 21:02:19 +0100

    first commit

---
+++ scrypt.js
@@ -0,0 +1,13 @@
+function addArticle1(){
+    alert("hello")
+// let nb= document.getElementById("span-price-1").value
+// let price=document.getElementById("price-article11").value
+// let priceAll=document.getElementById("price-all").value
+// nb+=1
+// price+=50
+// priceAll+=50
+// document.getElementById("span-price-1").value=nb
+// document.getElementById("price-article11").value=price
+// document.getElementById("price-all").value=priceAll
+
+} 
